ec6c79227a4a32174e0615a509f025b1b3327257
Aivanas <vk53418006@gmail.com>
2023-06-16 15:16:52 +0300

    sdsd

--- Person.cpp
+++ Person.cpp
@@ -4,3 +4,27 @@
 
 #include "Person.h"
 
+//
+// Created by richb on 16.06.2023.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+
+
+Person::Person (string name, unsigned short age, string role, string login, string password_hash)
+{
+    if (regex_match(name, pattern)){
+
+    }
+
+    this->name = name;
+    this->age = age;
+    this->role = role;
+    this->login = login;
+    this->password_hash = password_hash;
+}
+
--- Person.h
+++ Person.h
@@ -5,17 +5,35 @@
 #ifndef FINAL_PROJECT_PERSON_H
 #define FINAL_PROJECT_PERSON_H
 
-#include <iostream>
+#include <regex>
 #include <Windows.h>
+
 
 using namespace std;
 
+regex pattern("^[a-zA-Zа-яА-Я]+$");
+enum Role {
+    Guest,
+    SysAdmin,
+    StockMan,
+    Provider,
+    Accountant,
+    Chef,
+    Waiter,
+};
+
 class Person{
 private:
-    string name;
-    unsigned short  age;
+string name;
+unsigned short  age;
+string role;
+string login;
+string password_hash;
 
+public:
+Person(string name, unsigned short age, string role, string login, string password_hash);
 };
+
 
 
 #endif //FINAL_PROJECT_PERSON_H
